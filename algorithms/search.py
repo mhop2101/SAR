79e11542122bd82007fe08f356ae02193fedddc8
rooot <miguelagc@outlook.es>
2026-02-13 17:33:49 -0500

    Implement depth-first search algorithm and enhance tinyHouseSearch with debugging prints

--- algorithms/search.py
+++ algorithms/search.py
@@ -9,8 +9,12 @@ def tinyHouseSearch(problem: SearchProblem):
     Returns a sequence of moves that solves tinyHouse. For any other building, the
     sequence of moves will be incorrect, so only use this for tinyHouse.
     """
+    print("Start:", problem.getState)
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))  # Posibles movimientos
     s = Directions.SOUTH
     w = Directions.WEST
+    print("Cost of these moves:", problem.getCostOfActions([s, s, w, s, w, w, s, w]))
     return [s, s, w, s, w, w, s, w]
 
 
@@ -28,8 +32,33 @@ def depthFirstSearch(problem: SearchProblem):
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+    stack = utils.Stack()
+    actions = utils.Stack()
+    visited = set()
+    
+    stack.push(problem.getStartState())
+    actions.push([])
+    
+    
+    while not stack.isEmpty():
+        node = stack.pop()
+        action = actions.pop()
+        
+        if node not in visited:
+            visited.add(node)
+        
+        if problem.isGoalState(node):
+            # Llegamos al objetivo
+            return action
+        
+        for element in problem.getSuccessors(node):
+            if element[0] not in visited:
+                stack.push(element[0])
+                actions.push( action + [element[1]])
+        
+    
+    return []
 
 
 def breadthFirstSearch(problem: SearchProblem):
